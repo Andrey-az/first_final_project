9a788b4b8420399a2e29b647f4a9c4b7607519bd
Andrey-az <andrey_az@mail.ru>
2023-03-23 21:24:13 +1000

    Написали программу

--- Program.cs
+++ Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿string[] array1;
+Console.WriteLine("Введите массив строк через пробел");
+string text_a = Console.ReadLine()!;
+array1 = text_a.Split(' ');
+string[] array2 = new string[array1.Length];
+void NewArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+NewArray(array1, array2);
+PrintArray(array2);
